bf910ac22cc91e8bbd5b3e0d4e8a05366973936c
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-09-03 11:53:18 -0400

    change icon

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 
 const Navbar = ({ title, icon }) => {
   return (
-    <nav className='text-center md:flex flex-row justify-between items-center w-full mb-4 px-4 py-4 bg-gradient-to-b from-blue-700 to-blue-500 z-50 text-white'>
+    <nav className='text-center md:flex flex-row justify-between items-center w-full mb-4 px-4 py-4 bg-gray-700 z-50 text-white'>
       <h1 className='text-xl font-semibold'>
-        <i className={icon}></i> {title}
+        <i className={`text-2xl ${icon}`}></i> {title}
       </h1>
     </nav>
   );
@@ -13,7 +13,7 @@ const Navbar = ({ title, icon }) => {
 
 Navbar.defaultProps = {
   title: "Lightning Weather",
-  icon: "fas fa-bolt",
+  icon: "fas fa-cluod-sun",
 };
 
 Navbar.propTypes = {
