ff5a097fa4ba05c1210acd82359f3ac2a989966c
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-09-09 10:22:33 -0400

    remove border for search form for samll screens

--- src/components/city/Search.js
+++ src/components/city/Search.js
@@ -20,7 +20,7 @@ export class Search extends Component {
       <div>
         <form
           onSubmit={this.onSubmit}
-          className='flex justify-center border border-gray-200 bg-white p-4 mb-4'
+          className='flex justify-center lg:border border-gray-200 bg-white p-4 mb-4'
         >
           <input
             type='text'
@@ -28,12 +28,12 @@ export class Search extends Component {
             placeholder='Enter City...'
             value={this.state.text}
             onChange={this.onChange}
-            className='bg-gray-100 outline-none rounded-l-md border-2 border-gray-200 focus:ring-2 focus:ring-blue-400 focus:border-none placeholder-gray-500 focus:placeholder-blue-400 p-2'
+            className='bg-gray-100 outline-none rounded-l-md border-2 border-gray-200 focus:ring focus:ring-blue-400 focus:border-none placeholder-gray-500 focus:placeholder-blue-400 p-2 ml-2'
           />
           <input
             type='submit'
             value='Search'
-            className='appearance-none bg-blue-500 hover:bg-blue-700 text-white rounded-r-md p-2 px-4'
+            className='appearance-none bg-blue-500 hover:bg-blue-700 text-white rounded-r-md p-2 px-4 mr-2'
           />
         </form>
       </div>
