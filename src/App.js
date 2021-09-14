453dfa34217de9604ad88356abcf160384118d3d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-09-14 09:42:05 -0400

    catch an error/404 if entered city doesn't exist

--- src/App.js
+++ src/App.js
@@ -10,17 +10,25 @@ class App extends Component {
   state = {
     city: null,
     loading: false,
+    error: null,
   };
 
   // Search City
   searchCity = async (text) => {
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `https:\\api.openweathermap.org/data/2.5/weather?q=${text}&units=imperial&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
-    );
+    try {
+      const res = await axios.get(
+        `https:\\api.openweathermap.org/data/2.5/weather?q=${text}&units=imperial&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
+      );
 
-    console.log(res.data);
-    this.setState({ city: res.data, loading: false });
+      console.log(res.data);
+      this.setState({ city: res.data, loading: false });
+    } catch (err) {
+      console.log(`Error: ${err}`);
+      this.setState({
+        error:
+          "There are was an error receving data for your location. Please try again.",
+      });
+    }
   };
 
   // Clear City from state
@@ -28,13 +36,23 @@ class App extends Component {
     this.setState({ city: null, loading: false });
   };
 
+  // Set alert
+  setAlert = (msg) => {
+    this.setState({ alert: { msg } });
+  };
+
   render() {
-    const { city, loading } = this.state;
+    const { city, loading, error } = this.state;
 
     return (
       <div className='font-roboto bg-blue-50 bg-opacity-40 h-full'>
         <Navbar title='Lightning Weather' icon='fas fa-cloud-sun' />
         <div className='mx-auto md:w-2/5 px-4 md:px-8 text-center'>
+          {error && (
+            <div className='p-8 border border-0 border-red-500 text-gray-700'>
+              {error}
+            </div>
+          )}
           <Search
             searchCity={this.searchCity}
             clearCity={this.clearCity}
