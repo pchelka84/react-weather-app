2254e49b4cec5ff4800dc407bcf4c5ee72348874
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-09-14 11:51:28 -0400

    create about page/component

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1 className='text-2xl pb-6'>About This App</h1>
+      <p className='pb-4'>
+        App to check current weather in different cities around the world.
+      </p>
+      <p className='pb-4'>Version: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
