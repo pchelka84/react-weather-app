96d2bbde47e55c9613f77650f01b4a54f0a494aa
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-09-14 13:57:40 -0400

    add routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import City from "./components/city/City";
 import Search from "./components/city/Search";
 import ErrorAlert from "./components/layout/ErrorAlert";
+import About from "./components/pages/About";
 import axios from "axios";
 
 import "./App.css";
@@ -44,18 +46,31 @@ class App extends Component {
     const { city, loading, error } = this.state;
 
     return (
-      <div className='font-roboto bg-blue-50 bg-opacity-40 h-full'>
-        <Navbar title='Lightning Weather' icon='fas fa-cloud-sun' />
-        <div className='mx-auto md:w-2/5 px-4 md:px-8 text-center'>
-          <ErrorAlert errorMsg={error} />
-          <Search
-            searchCity={this.searchCity}
-            clearCity={this.clearCity}
-            showClearIcon={city ? true : false}
-          />
-          {city && <City {...city} loading={loading} />}
+      <Router>
+        <div className='font-roboto bg-blue-50 bg-opacity-40 h-full'>
+          <Navbar title='Lightning Weather' icon='fas fa-cloud-sun' />
+          <div className='mx-auto md:w-2/5 px-4 md:px-8 text-center'>
+            <ErrorAlert errorMsg={error} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchCity={this.searchCity}
+                      clearCity={this.clearCity}
+                      showClearIcon={city ? true : false}
+                    />
+                    {city && <City {...city} loading={loading} />}
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title, icon }) => {
   return (
     <nav className='text-center md:flex flex-row justify-between items-center w-full mb-6 px-4 py-4 bg-gray-700 z-50 text-white'>
-      <h1 className='text-xl font-semibold'>
+      <h1 className='md:text-xl font-semibold'>
         <i className={`text-2xl ${icon}`}></i> {title}
       </h1>
+      <ul className='flex justify-center'>
+        <li className='p-2 my-0 mx-1 hover:text-gray-300'>
+          <Link to='/'>Home</Link>
+        </li>
+        <li className='p-2 my-0 mx-1 hover:text-gray-300'>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
